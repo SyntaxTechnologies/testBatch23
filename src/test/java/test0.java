8cc13691334fba3effa1db522a2cb2458e968685
Syntax <instructor@syntaxtechs.com>
2025-05-14 21:46:37 -0400

    Update test0.java

--- src/test/java/test0.java
+++ src/test/java/test0.java
@@ -1,5 +1,5 @@
 public class test0 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("we are groooooooooooooooooooooooooooooooooooooooooot");
     }
 }
